4453e0778b9e16c25c835756122e1f5cd53fe4a4
Sebatian Lapke <Sebastian_lapke@icloud.com>
2022-05-28 00:50:35 +0200

    Add email send application logic

--- src/main/java/com/mail/client/MailclientApplication.java
+++ src/main/java/com/mail/client/MailclientApplication.java
@@ -1,37 +1,20 @@
 package com.mail.client;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-@SpringBootApplication
-public class MailclientApplication implements CommandLineRunner {
-	@Autowired
-	private JavaMailSender javaMailSender;
-	public static void main(String[] args) {
-		SpringApplication.run(MailclientApplication.class, args);
 
-	}
-	@Override
-	public void run(String... args) {
+public class MailclientApplication {
 
-		System.out.println("Sending Email...");
+	public void sendEmail(JavaMailSender javamailSender, String recipient, String content, String subject) {
 
-		sendEmail();
-
-		System.out.println("Done");
-
-	}
-
-	public void sendEmail() {
 		SimpleMailMessage msg = new SimpleMailMessage();
 		msg.setFrom("sebastian@lapke.info");
-		msg.setTo("sebastian_lapke@icloud.com");
+		msg.setTo(recipient);
 
-		msg.setSubject("Testing from Spring Boot");
-		msg.setText("Hello World \n Spring Boot Email");
-		javaMailSender.send(msg);
+		msg.setSubject(subject);
+		msg.setText(content);
+		javamailSender.send(msg);
 	}
 }
